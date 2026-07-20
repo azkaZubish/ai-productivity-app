9af38d9125eb93acec0aa8f299644e89810daf92
azkaZubish <azkazubish@gmail.com>
2026-07-20 12:46:47 +0530

    Successfully connected MongoDB Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
-
+require('dotenv').config()
 const app = require('./app')
+const connectDB = require('./config/db')
 const PORT = 3000
+
+connectDB()
 
 app.listen(PORT, () => {
     console.log(`Server running successfully on port ${PORT}`)
